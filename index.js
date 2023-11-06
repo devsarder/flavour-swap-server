0c7ffb388e4f74c6f0c294e2289ce6073d001f44
sujon sarder <SUJONSARDER92@GMAIL.COM>
2023-11-06 22:53:21 +0100

    Implemented a POST API for clients to submit new data to the server.

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = process.env.PORT || 3000;
 const app = express();
 
+const uri = `mongodb+srv://${process.env.Db_user}:${process.env.Db_pass}@cluster0.vdxshj0.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.Db_user);
+console.log(process.env.Db_pass);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const foodsCollection = client
+      .db("flavorSwap")
+      .collection("foodCollections");
+    // Send a ping to confirm a successful connection
+    app.post("/foods", async (req, res) => {
+      const food = req.body;
+      const result = await foodsCollection.insertOne(food);
+      res.send(result);
+    });
+    app.get("/foods", async (req, res) => {
+      const cursor = foodsCollection.find();
+      const result = await cursor.toArray(cursor);
+      res.send(result);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
